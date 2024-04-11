7e70fcebe8675e7673b1a263ff95cf0142de1794
Israel Pedrosa <36051117+iCPedrosa@users.noreply.github.com>
2024-04-10 21:45:03 -0400

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -6,6 +6,15 @@ $message = $_POST['message'];
 $formcontent=" From: $name  \n Message: $message";
 $subject = "Contact Form";
 $mailheader = "From: icpedrosa@swiftyresults.com \r\n";
-mail($email, $subject, $formcontent, $mailheader) or die("Error!");
+
+try {
+    mail($email, $subject, $formcontent, $mailheader) or die("Error!");
+} catch (Exception $ex) {
+	// jump to this part
+	// if an exception occurred
+    echo "$ex";
+    
+}
+
 echo "Thank You!";
 ?>
