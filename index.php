34a6b32e334baeed0eead3517ef4c7040b7dfb9c
Bernardo melo <49568557+bernardoxm@users.noreply.github.com>
2024-06-11 00:18:52 -0300

    logoswiftyresults

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
+        <img src="assets/img/logoswiftyresults.png" alt="">
         <span><b>Swifty</b>Results</span>
       </a>
 
