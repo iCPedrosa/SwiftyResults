5d0397c730b551ad9c8d11351c2eb492fdc4400a
Israel Pedrosa <36051117+iCPedrosa@users.noreply.github.com>
2024-06-10 22:22:49 -0400

    analytics

---
+++ php/analyticstracking.php
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DG39J6KWN1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DG39J6KWN1');
+</script>
