067c9f3397e6cabb62b9c93a7db55f5f3bb0046b
Bernardo melo <49568557+bernardoxm@users.noreply.github.com>
2024-06-11 00:02:46 -0300

    Edit email us

--- index.php
+++ index.php
@@ -1261,6 +1261,7 @@
                   <i class="bi bi-envelope"></i>
                   <h3>Email Us</h3>
                   <p>icpedrosa@swiftyresults.com</p>
+                  <p>bernardo.melo@swiftyresults.com</p>
                 </div>
               </div>
               <div class="col-md-6">
