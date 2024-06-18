f62f4886cf6074b634e457f9bf26e1b3ca85c261
Pedrosa, Israel <israel.pedrosa@hertz.com>
2024-06-17 22:54:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -140,14 +140,14 @@
       <div class="container" data-aos="fade-up">
         <div class="row gx-0">
 
-          <div class="col-lg-8 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
-            <div class="content text-center">
+          <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+            <div class="content">
               <h3>Who We Are</h3>
               <h2>We're all about creating amazing digital solutions that help your business shine online.</h2>
               <p>
               Whether you're looking for a stunning website, a seamless CRM system, or some marketing magic, we've got you covered. What sets us apart? Our friendly team's here to partner with you every step of the way. Let's make your online presence shine. <br/><br/>Welcome to SwiftyResults, where success is just a click away.
               </p>
-              <div class="text-center text-lg-start justify-content-center">
+              <div class="text-center text-lg-start">
                 <a href="#services" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
                   <span>Read More</span>
                   <i class="bi bi-arrow-right"></i>
@@ -156,9 +156,9 @@
             </div>
           </div>
 
-          <!-- <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+          <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
             <img src="assets/img/about.jpg" class="img-fluid" alt="">
-          </div> -->
+          </div>
 
         </div>
       </div>
