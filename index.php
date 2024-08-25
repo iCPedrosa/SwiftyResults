6816fc4a5831810f5a459e6a37f243940061f44b
Pedrosa, Israel <israel.pedrosa@hertz.com>
2024-08-24 23:56:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1717,43 +1717,6 @@
                 </div>
               </div><!-- End testimonial item -->
 
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <div class="stars">
-                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
-                      class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                  </div>
-                  <p>
-                    The SMS and email marketing campaigns executed by this team have yielded outstanding results for our
-                    business. Their strategic approach and attention to detail have significantly boosted our customer
-                    engagement and conversions. Highly recommended!
-                  </p>
-                  <div class="profile mt-auto">
-                    <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
-                    <h3>Daniel Adams</h3>
-                    <h4>Marketing Manager</h4>
-                  </div>
-                </div>
-              </div><!-- End testimonial item -->
-
-              <div class="swiper-slide">
-                <div class="testimonial-item">
-                  <div class="stars">
-                    <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i
-                      class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                  </div>
-                  <p>
-                    The website development service provided by this company surpassed all expectations. They were
-                    attentive to our needs, responsive to feedback, and delivered a website that perfectly reflects our
-                    brand. Exceptional work!
-                  </p>
-                  <div class="profile mt-auto">
-                    <img src="assets/img/testimonials/testimonials-5.jpg" class="testimonial-img" alt="">
-                    <h3>Michael Johnson</h3>
-                    <h4>CEO</h4>
-                  </div>
-                </div>
-              </div><!-- End testimonial item -->
 
 
             </div>
